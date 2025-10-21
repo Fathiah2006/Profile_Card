212961bfbed255f261b1131c7e2e69af444dacef
Fathiah <fathiahkazeem@gmail.com>
2025-10-21 17:32:40 +0100

    second commit

--- contact.js
+++ contact.js
@@ -129,18 +129,3 @@ form.addEventListener('submit', function(e) {
   }
 });
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.getElementById("hamburger");
-    const navMenu = document.getElementById("nav-menu");
-    const close = document.getElementById("close");
-
-    hamburger.addEventListener("click", function() {
-        navMenu.classList.toggle("active");
-    });
-
-    close.addEventListener("click", function() {
-        navMenu.classList.remove("active");
-    });
-});
